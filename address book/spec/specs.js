f4d6476528447bc1b490fb482a5be80e42a2f8b8
Peter Tinti & Joshua Thomas Paul <student@epicodus.com>
2014-02-25 10:21:13 -0800

    another address working in adress book phone numbers not yet added

--- address book/spec/specs.js
+++ address book/spec/specs.js
@@ -1,5 +1,11 @@
-describe('leapYear', function() {
-  it('Capitilises the first letter of every important word.', function() {
-    titleCase("james").should.equal("James");
-  });  
+describe('Address', function() {
+  describe("fullAddress", function() {
+    it("returns the full address the nice formatting", function() {
+      var testAddress = Object.create(Address);
+      testAddress.street = "1 Park Ave";
+      testAddress.city = "New York";
+      testAddress.state = "New York";
+      testAddress.fullAddress().should.equal("1 Park Ave, New York, New York");
+    });
+  });
 });
